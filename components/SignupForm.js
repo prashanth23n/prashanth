43f6da107110899139eb41a9154bbfeb19164eee
prashanth23n <142340966+prashanth23n@users.noreply.github.com>
2023-08-17 00:01:57 +0530

    Add files via upload

---
+++ components/SignupForm.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+const SignupForm = ({ onSignup }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform signup logic here
+    onSignup(email);
+  };
+
+  return (
+    <div>
+      <h2>Signup</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Signup</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignupForm;
